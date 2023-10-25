23f798c1992e448d5fc45a35df04a15ddc75e25f
Chru Brar <chrubrar@gmail.com>
2023-10-25 15:54:47 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a-us.storyblok.com",
+        port: "",
+        pathname: '/f/1017162/1145x529/420a63ed37/**',
+        
+      },
+     {
+        protocol: "https",
+        hostname: "a-us.storyblok.com",
+        port: "",
+        pathname: '/f/1017162/300x300/323bae3bb7/**',
+        },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
